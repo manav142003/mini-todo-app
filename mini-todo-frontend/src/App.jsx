9cebdce62ad68bc4a8292799d1137dde1668a0e3
Manav Patel <manavpatel489@gmail.com>
2026-02-13 12:08:06 +0530

    ci/cd

--- mini-todo-frontend/src/App.jsx
+++ mini-todo-frontend/src/App.jsx
@@ -57,7 +57,7 @@ function App() {
       <h1 style={{ textAlign: "center", color: "#5c48ee" }}>
        Manav Patel App
       </h1>
-
+{/*  */}
       <div style={{ display: "flex", marginBottom: "20px" }}>
         <input
           value={task}
